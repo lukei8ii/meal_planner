0ea85fa9845073a99cd738952bccd73b05524e19
Shea Hunter <contshun@nerdery.com>
2015-03-25 16:30:57 -0500

    added in-n-out

---
+++ data_in_n_out.js
@@ -0,0 +1,7 @@
+var RawItemData = RawItemData || {};
+
+RawItemData["In-N-Out"] = [
+	{ name: "2x2 Protein Style w/ onion", fat: 39, carbs: 11, protein: 33, calories: 520 },
+	{ name: "3x3 Protein Style w/ onion", fat: 53, carbs: 11, protein: 48, calories: 710 },
+	{ name: "4x4 Protein Style w/ onion", fat: 67, carbs: 11, protein: 63, calories: 900 }
+];
